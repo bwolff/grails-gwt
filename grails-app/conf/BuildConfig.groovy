dce4a37d0faf52072e6b6d00cd660095d0352f6f
Benjamin Wolff <benjamin.wolff@cern.ch>
2013-05-24 08:42:05 +0200

    Fixed spock dependency for Grails 2.2.x and Groovy 2.x

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,11 +15,19 @@ grails.project.dependency.resolution = {
         build "org.apache.ivy:ivy:2.2.0"
         build "org.grails.plugins:resources:1.2.RC2"
 
-
+        // We need to replace the spock 0.6 version that comes with the GWT plugin. Otherwise the tests fail with a compilation error.
+        // See documentation about Grails 2.2: http://grails.org/plugin/spock
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
     plugins {
-        test(":spock:0.6")
         build ":release:2.0.4"
+
+        // We need to replace the spock 0.6 version that comes with the GWT plugin. Otherwise the tests fail with a compilation error.
+        // See documentation about Grails 2.2: http://grails.org/plugin/spock
+        test(":spock:0.7") {
+            exclude "spock-grails-support"
+            export = false
+        }
     }
 }
 
