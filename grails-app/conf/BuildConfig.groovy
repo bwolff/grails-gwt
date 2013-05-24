3a0fce8265bf04ae02669e3857ac8fbc9d38cafb
Benjamin Wolff <benjamin.wolff@cern.ch>
2013-05-24 08:48:54 +0200

    Corrected resources plugin dependency and put it in the correct scope. Now the initial compilation error about the MapperPhase that occurs on a fresh compile is gone.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,15 +9,17 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        build "org.grails.plugins:resources:1.2.RC2"
-
         // We need to replace the spock 0.6 version that comes with the GWT plugin. Otherwise the tests fail with a compilation error.
         // See documentation about Grails 2.2: http://grails.org/plugin/spock
         test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
-        build ":release:2.2.1"
+        compile ":resources:1.2.RC2"
+
+        build(":release:2.2.1") {
+            export = false
+        }
 
         // We need to replace the spock 0.6 version that comes with the GWT plugin. Otherwise the tests fail with a compilation error.
         // See documentation about Grails 2.2: http://grails.org/plugin/spock
@@ -27,8 +29,6 @@ grails.project.dependency.resolution = {
         }
     }
 }
-
-grails.release.scm.enabled = false
 
 // Needed to run commands locally that trip over the GWT_HOME check.
 gwt { version = "2.4.0" }
