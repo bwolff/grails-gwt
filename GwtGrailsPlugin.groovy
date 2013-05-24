2a3a60c88b0911f6a3d745cf50f8e17ebbed02af
Benjamin Wolff <benjamin.wolff@cern.ch>
2013-05-24 13:49:37 +0200

    Changed the group ID to now collide with the original one. Adding the string ais into the version seems to cause problem when deploying to Archiva/Nexus.

--- GwtGrailsPlugin.groovy
+++ GwtGrailsPlugin.groovy
@@ -19,7 +19,8 @@ import org.codehaus.groovy.grails.plugins.gwt.DefaultGwtServiceInterfaceGenerato
 import org.codehaus.groovy.grails.web.plugins.support.WebMetaUtils
 
 class GwtGrailsPlugin {
-    def version = "0.8-ais-SNAPSHOT"
+    def groupId = "cern.ais"
+    def version = "0.9-SNAPSHOT"
     def grailsVersion = "2.2 > *"
     def title = "The Google Web Toolkit for Grails."
     def description = """\
